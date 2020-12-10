71d4da4bec2cc0075300d8a481160e41cfcf3eb9
Nasser Setti <Abdennasser.Essati@4d.com>
2020-12-10 15:47:23 +0100

    chore: add subscription to the app for real time approach

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 // import * as serviceWorker from './serviceWorker';
-
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 // 1
 import { setContext } from '@apollo/client/link/context';
 import {
@@ -30,11 +32,38 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// 3
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
+
+// 3
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
 
 // 4
 ReactDOM.render(
